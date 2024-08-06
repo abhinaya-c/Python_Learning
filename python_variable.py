564e53f23dac3225338edda4bd0f6de56358d4e4
abhinaya-c <abhinayachaurasiya@outlook.com>
2024-08-06 09:33:34 +0545

    Everything about variable

---
+++ python_variable.py
@@ -0,0 +1,45 @@
+#Variables:
+#varibles are containerd for storing data values.
+
+
+#Creating Variables:
+#Example:
+
+x = 5
+y = "WRC"
+print(x)
+print(y)
+
+#Changing Type after they have beem\n set.
+x = 4          #x is of type int 
+x = "NesCafe"  #x is now of type str
+print(x)
+
+
+#CASTING AND GET TYPE
+
+x = str(3)   # x will be '3'
+y = int(3)   # y will be 3
+z = float(3) # z will be 3.0
+
+#Get type
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Single or Double Quotes?
+
+x = "James"
+# Both are same 
+x = 'James'
+
+#Case-Sensitive
+#variable names are case-sensitive.
+
+#This will create two variables:
+a = 4
+A = "Simon"
+# A will not overwrite a 
+print(a)
+print(A)
