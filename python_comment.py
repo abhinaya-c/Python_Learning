ad399680f7c32252477f3a7af964ace7a047798d
abhinaya-c <abhinayachaurasiya@outlook.com>
2024-08-06 08:54:00 +0545

    Introduced multi-line comment

--- python_comment.py
+++ python_comment.py
@@ -3,3 +3,13 @@
 
 #This is a comment
 print("Hello, World")
+
+#print("HELLO, WORLD!")
+print("Cheers, Buddy!")
+
+
+#MULTI-LINE COMMENT
+#This is a comment
+#wrtten in 
+#more than just one line of
+print("Hello, World")
