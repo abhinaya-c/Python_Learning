65247f2d54948692330e9f873fed303c5268cdee
abhinaya-c <abhinayachaurasiya@outlook.com>
2024-08-09 00:30:21 +0545

    Introduced Basics of string.

---
+++ python_string.py
@@ -0,0 +1,39 @@
+print('Hello')
+print("hello")
+
+a = 'Hi, world'
+print(a)
+
+#Multi-line array
+b = """python is very useful language,
+can be used for many purposes"""
+print(b)
+
+#string as a array
+x = "Hello, World!"
+print(x[0])
+
+#looping through a string array
+
+for x in "banana":
+    print(x)
+
+#string length
+
+a = "Hello, World!"
+print(len(a))
+
+#check string
+txt ="The best thing in life are free!"
+print("free" in txt)
+
+#use it in an if statement
+if "free" in txt:
+    print("Yes, 'free' is present.")
+
+#check if Not 
+print("expensive" not in txt)
+
+if "expensive" not in txt:
+    print("No, 'expensive' is not present.")
+
